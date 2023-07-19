920bb251aa6399f7d342d700955a61d8f6786ac5
Oli Treadwell <oliver.treadwell@gmail.com>
2023-07-19 14:59:58 -0700

    initial config

---
+++ src/user-auth.js
@@ -0,0 +1,9 @@
+console.clear()
+console.log('user-auth.js: \n\n');
+
+// Design and Build a
+// User Authentication System
+// in JavaScript
+// See if you can use some of the 
+// concepts from Object Oriented Programming
+
